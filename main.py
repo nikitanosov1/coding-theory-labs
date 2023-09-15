1f180cec448a65f8cb1c6bdb4a136b0eec922a03
Никита Носов <darkxaril@yandex.ru>
2023-09-15 13:16:05 +0400

    remove mutation ref

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import numpy as np
 
 def swapRows(matrix, i, j):
-    matrix[[i, j]] = matrix[[j, i]]
-    return matrix
+    currentMatrix = matrix.copy()
+    currentMatrix[[i, j]] = currentMatrix[[j, i]]
+    return currentMatrix
 
 # Writes the xor result between firstRowIndex and secondRowIndex to the secondRowIndex
 def xorRowsInMatrix(matrix, firstRowIndex, secondRowIndex):
-    matrix[secondRowIndex] = np.logical_xor(matrix[firstRowIndex], matrix[secondRowIndex])
-    return matrix
+    currentMatrix = matrix.copy()
+    currentMatrix[secondRowIndex] = np.logical_xor(currentMatrix[firstRowIndex], currentMatrix[secondRowIndex])
+    return currentMatrix
 
 def ref(inputMatrix):
-    rows = inputMatrix.shape[0]
-    columns = inputMatrix.shape[1]
+    matrix = inputMatrix.copy()
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
 
     # Find and moving lead row
     indexForNextPositionOfLeadRow = 0
     indexColumnOfLeadingElement = -1
     while (True):
-        # print("indexForNextPositionOfLeadRow = ", indexForNextPositionOfLeadRow)
-        # print("indexColumnOfLeadingElement = ", indexColumnOfLeadingElement)
-        # print("inputMatrix = ", inputMatrix)
         isMoveLeadRow = False
         for i in range(indexColumnOfLeadingElement + 1, columns):
             for j in range(indexForNextPositionOfLeadRow, rows):
-                if inputMatrix[j][i]:
-                    inputMatrix = swapRows(inputMatrix, indexForNextPositionOfLeadRow, j)
+                if matrix[j][i]:
+                    matrix = swapRows(matrix, indexForNextPositionOfLeadRow, j)
                     indexForNextPositionOfLeadRow += 1
                     indexColumnOfLeadingElement = i
                     isMoveLeadRow = True
@@ -32,24 +32,25 @@ def ref(inputMatrix):
             if (isMoveLeadRow):
                 break
         if not isMoveLeadRow:
-            return inputMatrix[0:indexForNextPositionOfLeadRow]
+            return matrix[0: indexForNextPositionOfLeadRow]
 
         # Making xor with rows
         for j in range(indexForNextPositionOfLeadRow, rows):
-            if inputMatrix[j][indexColumnOfLeadingElement]:
-                inputMatrix = xorRowsInMatrix(inputMatrix, indexForNextPositionOfLeadRow - 1, j)
+            if matrix[j][indexColumnOfLeadingElement]:
+                matrix = xorRowsInMatrix(matrix, indexForNextPositionOfLeadRow - 1, j)
 
 if __name__ == '__main__':
-    # matrix1 = np.array([[False, False, True],
-    #                     [False,  True, True],
-    #                     [False,  True, False],
-    #                     [False,  True, True],
-    #                     ])
-    matrix1 = np.array([[True, False, True, True, False, False, False, True, False, False, True],
-                        [False, False, False, True, True, True, False, True, False, True, False],
-                        [False, False, False, False, True, False, False, True, False, False, True],
-                        [True, False, True, False, True, True, True, False, False, False, False],
-                        [False, False, False, False, True, False, False, True, True, True, False],
-                        [True, False, True, True, True, False, False, False, False, False, False]]
-                        )
+    matrix1 = np.array([[False, False, True],
+                        [False,  True, True],
+                        [False,  True, False],
+                        [False,  True, True],
+                        ])
+    # matrix1 = np.array([[True, False, True, True, False, False, False, True, False, False, True],
+    #                     [False, False, False, True, True, True, False, True, False, True, False],
+    #                     [False, False, False, False, True, False, False, True, False, False, True],
+    #                     [True, False, True, False, True, True, True, False, False, False, False],
+    #                     [False, False, False, False, True, False, False, True, True, True, False],
+    #                     [True, False, True, True, True, False, False, False, False, False, False]]
+    #                     )
     print(ref(matrix1))
+    print(matrix1)
